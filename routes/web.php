b956e8b943389b259fcf0fbe8ff061c3bc6c68f5
qwyqwyn <casiagwynethmarie@gmail.com>
2025-07-08 09:22:51 +0800

    try

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\Auth\ForgotPasswordController;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\PatientRecordController;
 
 
 Route::get('/', function () {
@@ -14,11 +16,15 @@ Route::get('/', function () {
 
 // Login routes
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'store'])->name('login');
+Route::post('/login', [AuthController::class, 'store'])->name('loginstore');
+
+Route::get('/two-factor-auth', [AuthController::class, 'showOtpForm'])->name('otp.form');
+Route::post('/two-factor-auth', [AuthController::class, 'verifyOtp'])->name('otp.verify');
+
+
 
 Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])
     ->name('password.request');
-
 Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])
     ->name('password.email');
 
@@ -32,20 +38,24 @@ Route::middleware('auth:admin')->group(function () {
     Route::post('/admin/logout', [AuthController::class, 'adminLogout'])->name('admin.logout');
 });
 
-// Admin dashboard routes (requires guard:admin) 
+// Admin dashboard routes (requires guard:admin)  
 Route::middleware('auth:patient')->group(function () {
     Route::get('/client/dashboard', [ClientController::class, 'index'])->name('client.dashboard');
     Route::post('/client/logout', [ClientController::class, 'clientLogout'])->name('client.logout');
-});
+}); 
  
 // Route for Patient Record
-Route::get('/patient-record', function () {
-    return view('admin.partials.patientRecord');
-})->name('patient.record');
+// Route::get('/patient-record', function () {
+//     return view('admin.patientRecord');
+// })->name('patientRecord');
+Route::get('/patientRecord', [PatientRecordController::class, 'index'])->name('admin.patientRecord');
 
-// Route for Medicine Record
+
+// // Route for Medicine Record
 Route::get('/medicine-record', function () {
-    return view('admin.partials.medicineRecord');
-})->name('medicine.record');
+    return view('admin.medicineRecord');
+})->name('medicineRecord');
 
-  
+Route::get('/staff-users', function () {
+    return view('admin.staffuser');
+})->name('admin.staffuser');
